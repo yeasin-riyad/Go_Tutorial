ff0af964e995ef091ba6cf8c41c5166a5b95204c
Yeasin-Riyad <mazumderyeasin98@gmail.com>
2026-03-27 19:58:49 +0600

    switchStatement

---
+++ switchStatement.go
@@ -0,0 +1,71 @@
+// single case switch statement
+// package main
+// import ("fmt")
+
+// func main() {
+//   day := 4
+
+//   switch day {
+//   case 1:
+//     fmt.Println("Monday")
+//   case 2:
+//     fmt.Println("Tuesday")
+//   case 3:
+//     fmt.Println("Wednesday")
+//   case 4:
+//     fmt.Println("Thursday")
+//   case 5:
+//     fmt.Println("Friday")
+//   case 6:
+//     fmt.Println("Saturday")
+//   case 7:
+//     fmt.Println("Sunday")
+//   }
+// }
+
+// with default case
+// package main
+// import ("fmt")
+
+// func main() {
+//   day := 8
+
+//   switch day {
+//   case 1:
+//     fmt.Println("Monday")
+//   case 2:
+//     fmt.Println("Tuesday")
+//   case 3:
+//     fmt.Println("Wednesday")
+//   case 4:
+//     fmt.Println("Thursday")
+//   case 5:
+//     fmt.Println("Friday")
+//   case 6:
+//     fmt.Println("Saturday")
+//   case 7:
+//     fmt.Println("Sunday")
+//   default:
+//     fmt.Println("Not a weekday")
+//   }
+// }
+
+
+// multiple case switch statement
+package main
+import ("fmt")
+
+func main() {
+   day := 5
+
+   switch day {
+   case 1,3,5:
+    fmt.Println("Odd weekday")
+   case 2,4:
+     fmt.Println("Even weekday")
+   case 6,7:
+    fmt.Println("Weekend")
+  default:
+    fmt.Println("Invalid day of day number")
+  }
+}
